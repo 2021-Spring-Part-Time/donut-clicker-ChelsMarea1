a89616632cb678ddb432e5eaa3141a39fe72f852
ChelsMarea1 <james.chelsea.m@gmail.com>
2021-07-09 16:12:56 -0400

    another-one

--- donut-maker.js
+++ donut-maker.js
@@ -7,16 +7,42 @@ export default class DonutMaker {
         this.numMultipliers = numMultipliers;
         this.costOfMultiplier = costOfMultiplier;
     }
+    /*
+getDonutClicks() {
+    return Math.round(this.donutClicks);
+}
+getNumberofDonuts() {
+    return Math.round(this.numDonuts);
+}
+getNumberOfAutoClickers() {
+    return Math.round(this.numAutoClickers);
+}
+getCostOfAutoClicker() {
+    return Math.round(this.costOfAutoClicker);
+}
+getNumberOfMultipliers () {
+    return Math.round(this.numMultipliers);
+}
+getCostOfMultiplier () {
+    return Math.round(this.costOfMultiplier);
+}
+*/
+
     addDonut() {
         this.numDonuts += Math.pow(1.2, this.numMultipliers);     
 
     }
+    //disableAutoClickerButton() {
+         
+  //  }
     addAutoClicker() {
+
         if (this.numDonuts >= 100) {
             this.numAutoClickers++;
             this.numDonuts -= 100;
             this.costOfAutoClicker = Math.round(this.costOfAutoClicker * 1.1);
         }
+
     }
     activateAutoClicker() {
             for (let i = 0; i<this.numAutoClickers;i++  ){
